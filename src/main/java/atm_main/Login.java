62dfd1c5812ead33b81726cf75345d4627ad2cb3
bircealptekin <bircetanil@gmail.com>
2021-07-10 17:23:29 +0300

    trying the login shit3

--- src/main/java/atm_main/Login.java
+++ src/main/java/atm_main/Login.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Scanner;
 
-import dto.CustomerDto;
 import utils.DbConnection;
 
 public class Login {
@@ -22,9 +21,7 @@ public class Login {
 	}
 	
 	public void checkLogin() {
-		String username = CustomerDto.getUsername(); //Assign user entered values to temporary variables.
-        String password = CustomerDto.getPassword();
-		
+	
 		Scanner input = new Scanner(System.in);
 		String usernameLogin, passwordLogin;
 		
@@ -35,23 +32,16 @@ public class Login {
 		
 		try (Connection connection = dbConnection()) {
 			PreparedStatement statement = (PreparedStatement) connection.prepareStatement("SELECT username, password FROM customers WHERE username=? AND password=?");
-			statement.setString(1, username);
-            statement.setString(2, password);
+			statement.setString(1, usernameLogin);
+            statement.setString(2, passwordLogin);
             ResultSet resultSet = statement.executeQuery();
-            while(resultSet.next()) // Until next row is present otherwise it return false
-            {
-             username = resultSet.getString("username"); //fetch the values present in database
-             password = resultSet.getString("password");
-
-              if(username.equals(usernameLogin) && password.equals(passwordLogin))
-              {
-                 System.out.println("Login successful."); ////If the user entered values are already present in the database, which means user has already registered so return a SUCCESS message.
-              }
-              else {
+            if(resultSet.next()) { // Until next row is present otherwise it return false
+            	System.out.println("Login successful.");
+            }
+            else {
             	  System.out.println("Invalid user credentials");
               }
 			
-            }
         } catch (Exception e) {
 			e.printStackTrace();
 		}
